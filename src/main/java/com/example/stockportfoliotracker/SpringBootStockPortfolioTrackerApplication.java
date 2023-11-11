5c14b42308fb67f87e09bbb9f2323eb8dd23bfff
pawelkozolub <pkozolub@gmail.com>
2023-11-11 20:07:26 +0100

    initial commit

---
+++ src/main/java/com/example/stockportfoliotracker/SpringBootStockPortfolioTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.stockportfoliotracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStockPortfolioTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStockPortfolioTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/stockportfoliotracker/SpringBootStockPortfolioTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stockportfoliotracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStockPortfolioTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
