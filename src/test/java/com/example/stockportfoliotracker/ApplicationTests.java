15fffa7084cbfde10f2540672f1a9e87b32ea9c6
pawelkozolub <pkozolub@gmail.com>
2023-11-12 16:26:39 +0100

    main app name change

--- src/test/java/com/example/stockportfoliotracker/ApplicationTests.java
+++ src/test/java/com/example/stockportfoliotracker/ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringBootStockPortfolioTrackerApplicationTests {
+class ApplicationTests {
 
     @Test
     void contextLoads() {
