8f29d9c35aa63625aaf0a7bcb9635433fea12894
pawelkozolub <pkozolub@gmail.com>
2023-11-13 22:39:07 +0100

    User and Role class for domain (model ) package

---
+++ src/main/java/com/example/stockportfoliotracker/domain/User.java
@@ -0,0 +1,24 @@
+package com.example.stockportfoliotracker.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    @ManyToMany(fetch = FetchType.EAGER)
+    private Collection<Role> roles = new ArrayList<>();
+}
