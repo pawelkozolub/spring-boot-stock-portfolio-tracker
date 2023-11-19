87da4b95ac29fc513f96eb1017c5fc40d096f92c
pawelkozolub <pkozolub@gmail.com>
2023-11-19 23:23:10 +0100

    mod 1. update RoleRepository -> Optional 2. update UserRepository -> Optional

--- src/main/java/com/example/stockportfoliotracker/repository/RoleRepository.java
+++ src/main/java/com/example/stockportfoliotracker/repository/RoleRepository.java
@@ -3,6 +3,8 @@ package com.example.stockportfoliotracker.repository;
 import com.example.stockportfoliotracker.domain.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByName(String name);
+    Optional<Role> findByAuthority(String authority);
 }
--- src/main/java/com/example/stockportfoliotracker/repository/UserRepository.java
+++ src/main/java/com/example/stockportfoliotracker/repository/UserRepository.java
@@ -3,6 +3,9 @@ package com.example.stockportfoliotracker.repository;
 import com.example.stockportfoliotracker.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
+    //User findByUsername(String username);
+    Optional<User> findByUsername(String username);
 }
