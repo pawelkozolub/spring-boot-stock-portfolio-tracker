704047f49894afd0445f772f17d84dadd0cbe70b
pawelkozolub <pkozolub@gmail.com>
2023-11-19 18:48:10 +0100

    base controllers for User and Admin roles

---
+++ src/main/java/com/example/stockportfoliotracker/controller/UserController.java
@@ -0,0 +1,17 @@
+package com.example.stockportfoliotracker.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    @GetMapping
+    public String helloUserController() {
+        return "User access level";
+    }
+}
