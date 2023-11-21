ce6f3acde90b7ae7fa5027fc0d45df84135fd1da
pawelkozolub <pkozolub@gmail.com>
2023-11-21 23:07:49 +0100

    add CommandLineRunner to set up database with roles and admin for application startup (create-drop database property used)

--- src/main/java/com/example/stockportfoliotracker/Application.java
+++ src/main/java/com/example/stockportfoliotracker/Application.java
@@ -1,13 +1,38 @@
 package com.example.stockportfoliotracker;
 
+import com.example.stockportfoliotracker.domain.Role;
+import com.example.stockportfoliotracker.domain.User;
+import com.example.stockportfoliotracker.repository.RoleRepository;
+import com.example.stockportfoliotracker.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    CommandLineRunner run(RoleRepository roleRepository, UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        return args -> {
+            if (roleRepository.findByAuthority("ADMIN").isPresent()) return;
+            Role adminRole = roleRepository.save(new Role(null, "ADMIN"));
+            Role userRole = roleRepository.save(new Role(null, "USER"));
+
+            Set<Role> roles = new HashSet<>();
+            roles.add(adminRole);
+
+            User admin = new User(null, "admin", passwordEncoder.encode("pwd"), roles);
+            userRepository.save(admin);
+        };
     }
 
 }
