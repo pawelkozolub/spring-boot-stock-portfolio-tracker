eba17a936ca83fd6a69f11e2c984bdb77dabe7f9
pawelkozolub <pkozolub@gmail.com>
2023-11-23 23:32:05 +0100

    mod HomeController to show JSP page

--- src/main/java/com/example/stockportfoliotracker/controller/HomeController.java
+++ src/main/java/com/example/stockportfoliotracker/controller/HomeController.java
@@ -1,17 +1,16 @@
 package com.example.stockportfoliotracker.controller;
 
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/")
 public class HomeController {
 
     @GetMapping
-    public ResponseEntity<String> home() {
-        return ResponseEntity.ok("Hello from the homepage");
+    public String home() {
+        return "home";
     }
 
 }
