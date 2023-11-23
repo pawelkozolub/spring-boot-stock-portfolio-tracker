604992a0579aedb788e3ac142b89a4547bb15fee
pawelkozolub <pkozolub@gmail.com>
2023-11-23 20:27:02 +0100

    add Repository annotation

--- src/main/java/com/example/stockportfoliotracker/repository/RoleRepository.java
+++ src/main/java/com/example/stockportfoliotracker/repository/RoleRepository.java
@@ -2,9 +2,11 @@ package com.example.stockportfoliotracker.repository;
 
 import com.example.stockportfoliotracker.domain.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByAuthority(String authority);
 }
--- src/main/java/com/example/stockportfoliotracker/repository/UserRepository.java
+++ src/main/java/com/example/stockportfoliotracker/repository/UserRepository.java
@@ -2,10 +2,11 @@ package com.example.stockportfoliotracker.repository;
 
 import com.example.stockportfoliotracker.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    //User findByUsername(String username);
     Optional<User> findByUsername(String username);
 }
