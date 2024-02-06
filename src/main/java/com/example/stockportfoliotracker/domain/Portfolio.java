4215c93c7dc1ae0bdceb85ae188ff5b17751af14
pawelkozolub <pkozolub@gmail.com>
2024-02-06 23:27:27 +0100

    Add Portfolio class used to have different stock strategies for balance and transaction comparison.

---
+++ src/main/java/com/example/stockportfoliotracker/domain/Portfolio.java
@@ -0,0 +1,35 @@
+package com.example.stockportfoliotracker.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "portfolios")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Portfolio {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @NotBlank
+    @Size(min = 1, max = 20)
+    private String name;
+    @Size(max = 600)
+    @Column(length = 1024)
+    private String description;
+    @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.REMOVE)
+    @JoinColumn(name = "portfolio_id")
+    private List<Transaction> transactions = new ArrayList<>();
+//    @OneToMany(mappedBy = "portfolio", cascade = CascadeType.REMOVE)
+//    private List<Balance> balances = new ArrayList<>();
+}
