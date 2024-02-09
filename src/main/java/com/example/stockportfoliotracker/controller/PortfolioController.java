ea761882780f7ee869189f0fb2d3cdb10d299d3a
pawelkozolub <pkozolub@gmail.com>
2024-02-09 23:26:02 +0100

    Refactor change name of HomeController to PortfolioController.

--- src/main/java/com/example/stockportfoliotracker/controller/PortfolioController.java
+++ src/main/java/com/example/stockportfoliotracker/controller/PortfolioController.java
@@ -1,5 +1,7 @@
 package com.example.stockportfoliotracker.controller;
 
+import com.example.stockportfoliotracker.domain.portfolio.Portfolio;
+import com.example.stockportfoliotracker.repository.PortfolioRepository;
 import com.example.stockportfoliotracker.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -8,12 +10,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import java.security.Principal;
 import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
-public class HomeController {
+public class PortfolioController {
 
     private final UserService userService;
+    private final PortfolioRepository portfolioRepository;
 
     @GetMapping("/")
     public String start() {
@@ -24,8 +28,9 @@ public class HomeController {
     public String home(Model model, Principal principal) {
         model.addAttribute("username", principal.getName());
         model.addAttribute("isAdmin", userService.checkIfAdmin(principal.getName()));
-        model.addAttribute("portfolioList", new ArrayList<Integer>());  // temporary
-        return "views/home";
+        List<Portfolio> portfolioList = portfolioRepository.findAll();
+        model.addAttribute("portfolioList", portfolioList);  // temporary
+        return "views/portfolio/home";
     }
 
 }
